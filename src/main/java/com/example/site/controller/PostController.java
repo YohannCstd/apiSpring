6ba3e8b6cf206f6fe3690f47d8b7edf56ef0df9d
YohannCstd <yohann.costard@ynov.com>
2022-11-22 12:48:56 +0100

    [dev] Modification de l'architecture de PostController et PostServices

--- src/main/java/com/example/site/controller/PostController.java
+++ src/main/java/com/example/site/controller/PostController.java
@@ -2,31 +2,29 @@ package com.example.site.controller;
 
 import com.example.site.entity.Post;
 import com.example.site.repository.PostRepository;
+import com.example.site.services.PostServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Objects;
-
-@RestController
 
 //Ajoute au début de la route "/api" pour ne plus avoir à mettre /api à chaque fois
+@RestController
 @RequestMapping("/api")
 public class PostController {
 
-    //PostRepository permet de faire le lien avec la table Post
+    @Autowired
+    private PostServices postServices;
     @Autowired
     private PostRepository postRepository;
 
     //GET
     //La route api/posts avec la méthode GET renvoie tout le contenu de la table Post avec la fonction findAll()
     @GetMapping("/posts")
-    public ResponseEntity<ArrayList<Post>> getAllPost() {
+    public ResponseEntity<?> getAllPost() {
+        if(postRepository.findAll() == null) return new ResponseEntity<>("Post not found !",HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(postRepository.findAll(), HttpStatus.OK);
     }
 
@@ -34,9 +32,9 @@ public class PostController {
     //Renvoie le Post correspondant à l'id passée en paramètre de l'URL
     //Le {id} se charge de recupérer le contenu présent après le /posts/ pour le passer en paramètre de le fonction
     @GetMapping("/posts/{id}")
-    public ResponseEntity<Post> getPost(@PathVariable long id) {
-        Post persistedPost = postRepository.findById(id);
-        return new ResponseEntity<>(persistedPost, HttpStatus.OK);
+    public ResponseEntity<?> getPost(@PathVariable long id) {
+        if(postRepository.findById(id) == null) return new ResponseEntity<>("Post not found !",HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(postRepository.findById(id), HttpStatus.OK);
     }
 
     //DELETE
@@ -44,7 +42,7 @@ public class PostController {
     //Renvoie Deleted si réussite, NOT FOUND si l'id n'est pas reconnu
     @DeleteMapping("/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable long id) {
-        if(postRepository.findById(id) == null)return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if(postRepository.findById(id) == null)return new ResponseEntity<>("Post not found !",HttpStatus.NOT_FOUND);
         postRepository.deleteById(postRepository.findById(id).getPostId());
         return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
@@ -54,52 +52,16 @@ public class PostController {
     //Renvoie BAD REQUEST si le titre est null
     //Renvoie OK Save with success ! si succès
     @PostMapping(value = "/posts")
-    public ResponseEntity<String> initPost(@RequestPart(value = "document", required = false)MultipartFile document, String title, String note) throws Exception {
-        //Vérifie une image est envoyée
-        if(document == null) return new ResponseEntity<>("Must send an image !",HttpStatus.BAD_REQUEST);
-        //Check if document is img
-        if(!Objects.equals(document.getContentType(), "image/jpeg")) return new ResponseEntity<>("Only JPEG !",HttpStatus.BAD_REQUEST);
-        if(title == null) return new ResponseEntity<>("Title is null",HttpStatus.BAD_REQUEST);
-        if(title.length() > 50) return new ResponseEntity<>("Title must be shorter than 50chr !", HttpStatus.BAD_REQUEST);
-        if(note != null) if(note.length() > 255) return new ResponseEntity<>("Note must be shorter than 255chr !", HttpStatus.BAD_REQUEST);
-        //Create path
-        Path currentRelativePath = Paths.get("");
-        String currentPath = currentRelativePath.toAbsolutePath().toString();
-        String realPathToUploads = currentPath + "\\src\\main\\resources\\uploads\\";
-        //Check if folder upload is already exist
-        if(! new File(realPathToUploads).exists())
-        {
-            new File(realPathToUploads).mkdir();
-        }
-        //New name
-        File folder = new File(realPathToUploads);
-        File[] files = folder.listFiles();
-        int count = files.length;
-        String orgName = count+1+".jpg";
-        //Destination
-        String filePath = realPathToUploads + orgName;
-        File dest = new File(filePath);
-        //Check if title null
-        //Save Post
-        postRepository.save(new Post(title,note,"localhost:9080/src/main/resources/uploads/"+orgName));
-        //Download img
-        document.transferTo(dest);
-        return new ResponseEntity<>("Save with success !", HttpStatus.OK);
+    public ResponseEntity initPost(@RequestPart(value = "document", required = false)MultipartFile document, String title, String note) throws Exception {
+        return ResponseEntity.ok(postServices.createPost(document,title,note));
     }
 
     //PUT
     //Modifie le POST (seulement titre et note) ayant comme id l'id passé en paramètre de l'URL
     //Renvoie Modified, OK si succès
     @PutMapping(value = "/posts/{id}")
-    public ResponseEntity<String> updatePost(@PathVariable long id, @RequestPart (value = "title", required = false) String title, String note) {
-        if(title == null) return new ResponseEntity<>("Title must be sent !", HttpStatus.BAD_REQUEST);
-        if(title.length() > 50) return new ResponseEntity<>("Title must be shorter than 50chr !", HttpStatus.BAD_REQUEST);
-        if(note.length() > 255) return new ResponseEntity<>("Note must be shorter than 255chr !", HttpStatus.BAD_REQUEST);
-        Post post = postRepository.findById(id);
-        post.setTitle(title);
-        post.setNote(note);
-        postRepository.save(post);
-        return new ResponseEntity<>("Modified", HttpStatus.OK);
+    public ResponseEntity updatePost(@PathVariable long id, @RequestPart (value = "title", required = false) String title, String note) {
+        return ResponseEntity.ok(postServices.update(id,title,note));
     }
 
 }
