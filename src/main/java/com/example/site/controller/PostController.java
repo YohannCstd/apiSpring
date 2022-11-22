2b695fbcf2af2f81093c1a982a8285438f55295d
Mathis Brouard <mathisbrouard@gmail.com>
2022-11-22 12:01:28 +0100

    PostController.java - COMMENTAIRE - finished

--- src/main/java/com/example/site/controller/PostController.java
+++ src/main/java/com/example/site/controller/PostController.java
@@ -59,6 +59,9 @@ public class PostController {
         if(document == null) return new ResponseEntity<>("Must send an image !",HttpStatus.BAD_REQUEST);
         //Check if document is img
         if(!Objects.equals(document.getContentType(), "image/jpeg")) return new ResponseEntity<>("Only JPEG !",HttpStatus.BAD_REQUEST);
+        if(title == null) return new ResponseEntity<>("Title is null",HttpStatus.BAD_REQUEST);
+        if(title.length() > 50) return new ResponseEntity<>("Title must be shorter than 50chr !", HttpStatus.BAD_REQUEST);
+        if(note != null) if(note.length() > 255) return new ResponseEntity<>("Note must be shorter than 255chr !", HttpStatus.BAD_REQUEST);
         //Create path
         Path currentRelativePath = Paths.get("");
         String currentPath = currentRelativePath.toAbsolutePath().toString();
@@ -77,7 +80,6 @@ public class PostController {
         String filePath = realPathToUploads + orgName;
         File dest = new File(filePath);
         //Check if title null
-        if(title == null) return new ResponseEntity<>("Title is null",HttpStatus.BAD_REQUEST);
         //Save Post
         postRepository.save(new Post(title,note,"localhost:9080/src/main/resources/uploads/"+orgName));
         //Download img
@@ -89,10 +91,10 @@ public class PostController {
     //Modifie le POST (seulement titre et note) ayant comme id l'id passé en paramètre de l'URL
     //Renvoie Modified, OK si succès
     @PutMapping(value = "/posts/{id}")
-    public ResponseEntity<String> updatePost(@PathVariable long id, @RequestPart String title, String note) {
-        if(title == null) {
-            return new ResponseEntity<>("Le titre doit être renseigné", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<String> updatePost(@PathVariable long id, @RequestPart (value = "title", required = false) String title, String note) {
+        if(title == null) return new ResponseEntity<>("Title must be sent !", HttpStatus.BAD_REQUEST);
+        if(title.length() > 50) return new ResponseEntity<>("Title must be shorter than 50chr !", HttpStatus.BAD_REQUEST);
+        if(note.length() > 255) return new ResponseEntity<>("Note must be shorter than 255chr !", HttpStatus.BAD_REQUEST);
         Post post = postRepository.findById(id);
         post.setTitle(title);
         post.setNote(note);
